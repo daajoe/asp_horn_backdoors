fd4705d5258eec98d4ce995f4d78b238e63611ef
Johannes Fichte <fichte@cs.uni-potsdam.de>
2015-09-17 18:51:42 +0200

    Constraint fix

--- horn_backdoor_cplex.py
+++ horn_backdoor_cplex.py
@@ -1,7 +1,11 @@
 from itertools import combinations,compress,imap, ifilter,izip
 import logging
+import logging.config
+logging.config.fileConfig('logging.conf')
+
 import cplex
 from cplex.exceptions import CplexError
+from lp_parse import Constraint
 
 def add_vars(l,ilp):
     lb=[0.0]*len(l.symtab.tab.keys())
@@ -11,12 +15,11 @@ def add_vars(l,ilp):
     ilp.variables.add(obj=obj,types=types, names=map(str,l.symtab.tab.keys()))
     
 def add_edge_constraint(l,ilp,v1,v2):
-    logging.debug('%s-%s', l.symtab.tab[v1],l.symtab.tab[v2])
+    logging.debug('%s-%s', l.symtab.tab.get(v1,None),l.symtab.tab.get(v2,None))
     vars = map(str,[v1,v2])
     ilp.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = vars, val = [1, 1])],
                                senses='G', 
                                rhs = [1],
-                               #range_values=[0.0], #)
                                names=['e(%s,%s)'%tuple(vars)])
 
 def define_ilp(l):
@@ -30,7 +33,7 @@ def define_ilp(l):
         for a1, a2 in combinations(r.head,2):
             add_edge_constraint(l,ilp,a1,a2)
         #add pairwise body
-        for a,c in izip(r.head,imap(lambda x: -x, neg)):
+        for a,c in izip(ifilter(lambda x: l.symtab.tab.get(x,None),r.head),imap(lambda x: -x, neg)):
             add_edge_constraint(l,ilp,a,c)
     return ilp
 
@@ -45,7 +48,9 @@ def compute_backdoor(l):
         logging.info('Instance solved.')
         logging.info(ilp.solution.get_status())
         logging.info(ilp.solution.status[ilp.solution.get_status()])
-        logging.info('Solution value  = %s', ilp.solution.get_objective_value())
+        logging.warning('='*80)        
+        logging.warning('Solution value  = %s', ilp.solution.get_objective_value())
+
         return compress(imap(lambda x: l.symtab.tab[int(x)],ilp.variables.get_names()),ilp.solution.get_values())
     except CplexError, e:
         logging.error(e)
