eed19d72a61fca78e07225de06785a6c9864b0b8
Johannes Fichte <daajoe@ficht.es>
2015-11-06 15:17:07 +0100

    Cplex variable fix

--- horn_backdoor_cplex.py
+++ horn_backdoor_cplex.py
@@ -7,24 +7,26 @@ import cplex
 from cplex.exceptions import CplexError
 
 def add_vars(l,ilp):
-    #L=range(max(l.symtab.tab.keys()))
     lb=[0.0]*len(l.symtab.tab.keys())
     ub=[1.0]*len(l.symtab.tab.keys())
     obj=[1.0]*len(l.symtab.tab.keys())
     types=["B"] * len(l.symtab.tab.keys())
     ilp.variables.add(obj=obj,types=types, names=map(str,l.symtab.tab.keys()))
+    return set(l.symtab.tab.keys())
     
-def add_var(v,ilp):
-    ilp.variables.add(obj=[1.0],types=['B'], names=[v])
+def add_var(v,ilp,cplex_vars):
+    if v not in cplex_vars:
+        ilp.variables.add(obj=[1.0],types=['B'], names=[v])
+        cplex_vars.add(v)
 
-def add_unnamed_symbols(v,l,ilp):
+def add_unnamed_symbols(v,l,ilp,cplex_vars):
     if not l.symtab.tab.get(v,None):
-        add_var(l.symtab[v][2:],ilp)
+        add_var(l.symtab[v][2:],ilp,cplex_vars)
 
-def add_edge_constraint(l,ilp,v1,v2):
+def add_edge_constraint(l,ilp,cplex_vars,v1,v2):
     logging.debug('%s-%s', l.symtab[v1],l.symtab[v2])
     vars_ = map(str,[v1,v2])
-    map(lambda x: add_unnamed_symbols(x,l,ilp), vars_)
+    map(lambda x: add_unnamed_symbols(x,l,ilp,cplex_vars), vars_)
     ilp.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = vars_, val = [1, 1])],
                                senses='G', 
                                rhs = [1],
@@ -32,17 +34,18 @@ def add_edge_constraint(l,ilp,v1,v2):
 
 def define_ilp(l):
     ilp = cplex.Cplex()
-    add_vars(l,ilp)
+    cplex_vars=add_vars(l,ilp)
     ilp.objective.set_sense(ilp.objective.sense.minimize)
 
     for r in l.statements:
-        neg = filter(lambda x: x<0,r.body)
         #add pairwise head
         for a1, a2 in combinations(r.head,2):
-            add_edge_constraint(l,ilp,a1,a2)
+            add_edge_constraint(l,ilp,cplex_vars,a1,a2)
         #add pairwise body
-        for a,c in izip(ifilter(lambda x: l.symtab[x],r.head),imap(lambda x: -x, neg)):
-            add_edge_constraint(l,ilp,a,c)
+        for a in ifilter(lambda x: l.symtab[x],r.head):
+            neg = filter(lambda x: x<0,r.body)
+            for c in imap(lambda x: -x, neg):
+                add_edge_constraint(l,ilp,cplex_vars,a,c)
     return ilp
 
 
@@ -55,7 +58,7 @@ def compute_backdoor(l,threads):
             ilp.parameters.threads.set(threads)
         ilp.solve()
         logging.info('Instance solved.')
-        logging.info(ilp.solution.get_status())
+        logging.info('Status = %n', ilp.solution.get_status())
         logging.info(ilp.solution.status[ilp.solution.get_status()])
         logging.warning('='*80)        
         logging.warning('Solution value  = %s', ilp.solution.get_objective_value())
